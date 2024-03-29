6f439e7113a5d69b70664842dd64d4bc99bf883f
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2024-03-29 18:11:40 +0000

    Rename KeyType, ValueType

--- include/rs/multi_map.h
+++ include/rs/multi_map.h
@@ -11,13 +11,13 @@ namespace rs {
 
 // A drop-in replacement for std::multimap. Internally creates a sorted copy of
 // the data.
-template <class KeyType, class ValueType>
+template <class RsKeyType, class RsValueType>
 class MultiMap {
  public:
   // Member type definitions.
-  using key_type = KeyType;
-  using mapped_type = ValueType;
-  using value_type = std::pair<KeyType, ValueType>;
+  using key_type = RsKeyType;
+  using mapped_type = RsValueType;
+  using value_type = std::pair<RsKeyType, RsValueType>;
   using size_type = std::size_t;
   using iterator = typename std::vector<value_type>::iterator;
   using const_iterator = typename std::vector<value_type>::const_iterator;
@@ -28,8 +28,8 @@ class MultiMap {
            size_t max_error = 32);
 
   // Lookup functions, like in std::multimap.
-  const_iterator find(KeyType key) const;
-  const_iterator lower_bound(KeyType key) const;
+  const_iterator find(RsKeyType key) const;
+  const_iterator lower_bound(RsKeyType key) const;
 
   // Iterators.
   const_iterator begin() const { return data_.begin(); }
@@ -40,18 +40,18 @@ class MultiMap {
 
  private:
   std::vector<value_type> data_;
-  RadixSpline<KeyType> rs_;
+  RadixSpline<RsKeyType> rs_;
 };
 
-template <class KeyType, class ValueType>
+template <class RsKeyType, class RsValueType>
 template <class BidirIt>
-MultiMap<KeyType, ValueType>::MultiMap(BidirIt first, BidirIt last,
+MultiMap<RsKeyType, RsValueType>::MultiMap(BidirIt first, BidirIt last,
                                        size_t num_radix_bits,
                                        size_t max_error) {
   // Empty spline.
   if (first == last) {
-    rs::Builder<KeyType> rsb(std::numeric_limits<KeyType>::min(),
-                             std::numeric_limits<KeyType>::max(),
+    rs::Builder<RsKeyType> rsb(std::numeric_limits<RsKeyType>::min(),
+                             std::numeric_limits<RsKeyType>::max(),
                              num_radix_bits, max_error);
     rs_ = rsb.Finalize();
     return;
@@ -59,7 +59,7 @@ MultiMap<KeyType, ValueType>::MultiMap(BidirIt first, BidirIt last,
 
   // Copy data and check if sorted.
   bool is_sorted = true;
-  KeyType previous_key = first->first;
+  RsKeyType previous_key = first->first;
   for (auto current = first; current != last; ++current) {
     is_sorted &= current->first >= previous_key;
     previous_key = current->first;
@@ -77,7 +77,7 @@ MultiMap<KeyType, ValueType>::MultiMap(BidirIt first, BidirIt last,
   // Create spline builder.
   const auto min_key = data_.front().first;
   const auto max_key = data_.back().first;
-  rs::Builder<KeyType> rsb(min_key, max_key, num_radix_bits, max_error);
+  rs::Builder<RsKeyType> rsb(min_key, max_key, num_radix_bits, max_error);
 
   // Build the radix spline.
   for (const auto& iter : data_) {
@@ -86,20 +86,20 @@ MultiMap<KeyType, ValueType>::MultiMap(BidirIt first, BidirIt last,
   rs_ = rsb.Finalize();
 }
 
-template <class KeyType, class ValueType>
-typename MultiMap<KeyType, ValueType>::const_iterator
-MultiMap<KeyType, ValueType>::lower_bound(KeyType key) const {
+template <class RsKeyType, class RsValueType>
+typename MultiMap<RsKeyType, RsValueType>::const_iterator
+MultiMap<RsKeyType, RsValueType>::lower_bound(RsKeyType key) const {
   SearchBound bound = rs_.GetSearchBound(key);
   return std::lower_bound(data_.begin() + bound.begin,
                           data_.begin() + bound.end, key,
-                          [](const value_type& lhs, const KeyType& rhs) {
+                          [](const value_type& lhs, const RsKeyType& rhs) {
                             return lhs.first < rhs;
                           });
 }
 
-template <class KeyType, class ValueType>
-typename MultiMap<KeyType, ValueType>::const_iterator
-MultiMap<KeyType, ValueType>::find(KeyType key) const {
+template <class RsKeyType, class RsValueType>
+typename MultiMap<RsKeyType, RsValueType>::const_iterator
+MultiMap<RsKeyType, RsValueType>::find(RsKeyType key) const {
   auto iter = lower_bound(key);
   return iter != data_.end() && iter->first == key ? iter : data_.end();
 }
